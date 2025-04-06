c658620310acdd0f5c11732d6f822b407ab7e24d
usanCode <sabinerugugula@gmail.com>
2025-04-06 20:13:51 +0200

    Answeting the first question of the assignment.

---
+++ answers.py
@@ -0,0 +1,20 @@
+
+# Create a program that reads a file and writes a modified version to a new file.
+
+# with open("./essai.txt", "r") as file:
+#    content = file.read()
+#    print(content)
+
+#with open("output.txt", "w") as file: file.write("Hello, Python! This is a test file.")
+# exception handling
+
+
+# Create a program that reads a file and writes a modified version to a new file.
+
+with open("week4PythonAssignment/essai.txt", "r") as file:
+    content = file.read()
+    print(content)
+
+with open("result.txt", "w") as file: 
+    file.write("My name is Usanase.")
+    file.write("I am happy to be here")
