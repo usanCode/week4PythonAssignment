8109d54a2f8830f963e6b64225d8d2679ee89a1d
usanCode <sabinerugugula@gmail.com>
2025-04-06 22:44:10 +0200

    answering the second question.

--- answers.py
+++ answers.py
@@ -12,3 +12,13 @@ with open("result.txt", "w") as file:
     file.write("I am happy to be here")
 
 # Error Handling Lab 🧪: Ask the user for a filename and handle errors if it doesn’t exist or can’t be read.
+
+filename = input("Enter the filename you want to open: ")
+
+try:
+    with open(filename, "r") as file:
+        content = file.read()
+        print("File content:")
+        print(content)
+except FileNotFoundError:
+    print("File not found. Please check the filename.")
